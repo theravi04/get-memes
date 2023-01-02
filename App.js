5bdf3f18c5459d218a02aaa92678ecb9fb5c4412
Ravi Kumar <ravi.rks4488@gmail.com>
2023-01-02 12:17:46 +0530

    MemeGenerator

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+
+  const [myMemes,setMyMemes] = useState('')
+
+  useEffect(() => {
+    fetchMemes()
+  
+  }, [])
+  
+  const fetchMemes = () => {
+    fetch ('https://ronreiter-meme-generator.p.rapidapi.com/meme?top=Top%20Text&bottom=Bottom%20Text&meme=Condescending-Wonka&font_size=50&font=Impact')
+      .then(response => response.json())
+      .then(json => {
+        const {myMemes} = json
+        setMyMemes(myMemes)
+      })
+      .catch((error) => console.error(error))
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {{Color:'black',
+                      justifyContent:'center'}}>
+        {myMemes}
+      </Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
