7fdb2acb6d800e448c99a64b12747e8b041e1159
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2023-01-02 16:56:22 +0530

    styles are left for Madhur vatsal Bharti to add

--- App.js
+++ App.js
@@ -1,40 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet, Text, View, Image, Button } from 'react-native-web';
 
 export default function App() {
 
   const [myMemes,setMyMemes] = useState('')
-
+  const [img, setImg] = useState("")
   useEffect(() => {
     fetchMemes()
   
   }, [])
-  
   const fetchMemes = () => {
-    fetch ('https://ronreiter-meme-generator.p.rapidapi.com/meme?top=Top%20Text&bottom=Bottom%20Text&meme=Condescending-Wonka&font_size=50&font=Impact')
-      .then(response => response.json())
+    fetch ('https://eager-meitner-f8adb8.netlify.app/.netlify/functions/random')
+      .then(res => res.json())
       .then(json => {
-        const {myMemes} = json
-        setMyMemes(myMemes)
+        const {title, url} = json;
+        setMyMemes(title)
+        setImg(url)
+        console.log(json);
+        console.log(url)
       })
       .catch((error) => console.error(error))
   }
   return (
     <View style={styles.container}>
-      <Text style = {{Color:'black',
-                      justifyContent:'center'}}>
-        {myMemes}
-      </Text>
+      <Text>{myMemes}</Text>
+      <Button onPress={fetchMemes} title="Get Meme"></Button>
+
+          {img ? (
+            <Image source={{ uri: img }} style={{height: 1600, width:1600,transform:[{scale:0.5}]}} />
+          ) : (
+            <></>
+          )}
+          
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
